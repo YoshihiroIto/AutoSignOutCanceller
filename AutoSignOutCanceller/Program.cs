83174a1d18a1e1e9a58e81fbd792b1439a31faa7
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2023-01-26 18:26:13 +0900

    Change target framework version(7.0 to 6.0)

--- AutoSignOutCanceller/Program.cs
+++ AutoSignOutCanceller/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Runtime.InteropServices;
 
 [DllImport("user32.dll", CharSet = CharSet.Auto)]
-static extern void keybd_event(byte bVirtualKey, byte bScan, uint dwFlags, nint dwExtraInfo);
+static extern void keybd_event(byte bVirtualKey, byte bScan, uint dwFlags, IntPtr dwExtraInfo);
 
 const byte VK_F24 = 0x87;
 const uint KEYEVENTF_KEYUP = 2;
@@ -10,9 +10,9 @@ while (true)
 {
     await Task.Delay(TimeSpan.FromSeconds(Random.Shared.Next(60 * 10, 60 * 11)));
 
-    keybd_event(VK_F24, 0, 0, nint.Zero);
+    keybd_event(VK_F24, 0, 0, IntPtr.Zero);
     await Task.Delay(TimeSpan.FromMilliseconds(Random.Shared.Next(50)));
-    keybd_event(VK_F24, 0, KEYEVENTF_KEYUP, nint.Zero);
+    keybd_event(VK_F24, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
 
     Console.WriteLine("AutoSignOutCanceller:update");
 }
